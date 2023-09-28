1b820992817589c55adae4cc7404039633b774cf
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-28 11:11:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(string word) {
+	cout << word << endl;
+}
+
+void print(int word) {
+	cout << word << endl;
+}
+
+void myfunc(int a, int b) {
+	int res1 = a + b;
+	print(res1);
+}
+
+void myfunc(int a, int b, int y) {
+	int res1 = a + b + y;
+	print(res1);
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	
+	print("youtube");
+	myfunc(3, 3);
+	myfunc(5, 5, 5);
+
+	return 0;
+}
